6bb85e9b5ecfa326d1bdbf7616074aa582612f62
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-09-09 22:28:19 +0300

    project

--- back-end/app/models/breweries-server1/app/models/brewery.rb
+++
@@ -1,4 +0,0 @@
-class Brewery < ActiveRecord::Base
-    belongs_to :retail
-    belongs_to :wholesale
-end
--- back-end/app/models/breweries-server1/app/models/wholesale.rb
+++
@@ -1,4 +0,0 @@
-class Wholesale < ActiveRecord::Base
-    has_many :breweries
-    has_many :retails, through: :breweries
-end
